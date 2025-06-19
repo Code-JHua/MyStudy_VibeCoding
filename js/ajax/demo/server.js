80c46424dce4f251c242f8e2284e15c1e0c45aa0
Hua <hjh17607090915@gmail.com>
2025-06-19 21:35:08 +0800

    ajax

---
+++ js/ajax/demo/server.js
@@ -0,0 +1,22 @@
+// 后端
+const http = require('http')
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    
+    if (req.url.startsWith('/login')) {
+        const username = req.url.split('?')[1].split('=')[1]
+        const password = req.url.split('&')[1].split('=')[1]
+
+        console.log(`当前登录账号为:${username},当前登录密码为:${password}`);
+
+        res.end('登录成功')
+    }
+})
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+})
+
+// 前端
---
+++ js/ajax/readme.md
@@ -0,0 +1,8 @@
+# 全栈
+前端项目 + 后端项目
+
+# 前后端沟通的桥梁
+- AJAX的全称是 **Asynchronous JavaScript and XML**（异步JavaScript和XML）。这是一种在不重新加载整个页面的情况下，与服务器交换数据并更新部分网页的技术。
+
+1. new XMLHttpRequest()
+2. fetch() --- 基于 promise 实现的 HTTP 请求，更现代，更强大
