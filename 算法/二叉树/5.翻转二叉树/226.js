7cde6ddf712b6405395c0663b3ea0baf162a3834
Hua <hjh17607090915@gmail.com>
2025-07-15 23:05:34 +0800

    翻转二叉树

---
+++ 算法/二叉树/5.翻转二叉树/226.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (!root) return root
+  reverse(root)
+
+  invertTree(root.left)
+  invertTree(root.right)
+  return root
+};
+
+var reverse = function (node) {
+  [node.left, node.right] = [node.right, node.left]
+}
